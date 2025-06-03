ca4198ae6cff6e39218678e076f91e6b937a4821
umpi-umpika <m.umpika@outlook.com>
2025-06-03 10:16:28 +0700

    Gravity flip done

--- Gravity_Flip/main.ts
+++ Gravity_Flip/main.ts
@@ -1,8 +1,9 @@
 export const flip = (direction: string, array: number[]): number[] => {
-    if(direction == "R") {
-        return [1, 3, 7, 9]
-    }
-    if(direction == "L") {
-        return [9, 7, 3, 1]
-    }
-}
+  const result = array.sort((a: number, b: number): number => a - b);
+  if (direction == "R") {
+    return result;
+  } else if (direction == "L") {
+    return result.reverse();
+  }
+  return [];
+};
